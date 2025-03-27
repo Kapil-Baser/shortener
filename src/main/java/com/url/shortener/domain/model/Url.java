aadbc17a21783a62375d38457d7c6e8fae764b3d
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-03-27 17:04:49 +0530

    ...

--- src/main/java/com/url/shortener/domain/model/Url.java
+++ src/main/java/com/url/shortener/domain/model/Url.java
@@ -1,4 +1,50 @@
 package com.url.shortener.domain.model;
 
+import java.time.LocalDateTime;
+
+
 public class Url {
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Url() {}
+
+    public Url(String originalUrl, String shortUrl) {
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
