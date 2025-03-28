ba1608ab205ca09e1e75126333dd04901e50ef5d
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-03-28 14:26:41 +0530

    Injecting the UrlShorteningFactory into controller

--- src/main/java/com/url/shortener/infrastructure/web/UrlShortenerController.java
+++ src/main/java/com/url/shortener/infrastructure/web/UrlShortenerController.java
@@ -1,5 +1,7 @@
 package com.url.shortener.infrastructure.web;
 
+import com.url.shortener.domain.factory.UrlShorteningFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class UrlShortenerController {
+
+    private final UrlShorteningFactory urlShorteningFactory;
+
+    @Autowired
+    public UrlShortenerController(UrlShorteningFactory factory) {
+        this.urlShorteningFactory = factory;
+    }
 
     @PostMapping("/shorten")
     public String shortenUrl(@RequestParam String url, @RequestParam(defaultValue = "base62") String strategy) {
