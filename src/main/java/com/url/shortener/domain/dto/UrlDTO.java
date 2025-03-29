4c4bbde644a7bc87d75dcf0e9032a362093f43c9
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-03-29 11:47:45 +0530

    Url DTO

---
+++ src/main/java/com/url/shortener/domain/dto/UrlDTO.java
@@ -0,0 +1,40 @@
+package com.url.shortener.domain.dto;
+
+public class UrlDTO {
+    private String id;
+    private String originalUrl;
+    private String shortUrl;
+    private String createdAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
