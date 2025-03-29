c92081890aa65fe5885da90c4904cf45ff0dd693
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-03-29 11:48:42 +0530

    Now saving the url to database and doing a lookup before trying to generate a url

--- src/main/java/com/url/shortener/domain/service/UuidShortener.java
+++ src/main/java/com/url/shortener/domain/service/UuidShortener.java
@@ -1,17 +1,42 @@
 package com.url.shortener.domain.service;
 
+import com.url.shortener.domain.model.Url;
+import com.url.shortener.infrastructure.persistence.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UuidShortener implements ShortenerService{
+    private final UrlRepository repository;
+
+    @Autowired
+    public UuidShortener(UrlRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public String generateShortUrl(String originalUrl) {
         try {
             String safeUrl = UrlSanitizer.sanitizeUrl(originalUrl);
-            return UUID.randomUUID().toString().replace("-", "").substring(0, 6);
+
+            Optional<Url> existingUrl = repository.findByOriginalUrl(safeUrl);
+            if (existingUrl.isPresent()) {
+                return existingUrl.get().getShortUrl();
+            }
+
+            String shortUrl = UUID.randomUUID().toString().replace("-", "").substring(0, 6);
+
+            Url generatedUrl = new Url(safeUrl, shortUrl);
+            repository.save(generatedUrl);
+
+            return shortUrl;
         } catch (IllegalArgumentException e) {
             throw new RuntimeException("URL Validation error: " + e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected error while saving URL: " + e.getMessage());
         }
     }
 }
