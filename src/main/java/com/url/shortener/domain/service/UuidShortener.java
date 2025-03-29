11684d103b964502c85565de7783e4b1b675d7b5
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-03-29 18:18:25 +0530

    now UuidShortener returns a UrlDTO object instead of a string

--- src/main/java/com/url/shortener/domain/service/UuidShortener.java
+++ src/main/java/com/url/shortener/domain/service/UuidShortener.java
@@ -1,16 +1,22 @@
 package com.url.shortener.domain.service;
 
+import com.url.shortener.domain.dto.UrlDTO;
+import com.url.shortener.domain.mapper.UrlMapper;
 import com.url.shortener.domain.model.Url;
 import com.url.shortener.infrastructure.persistence.UrlRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UuidShortener implements ShortenerService{
     private final UrlRepository repository;
+    private final Logger logger = LoggerFactory.getLogger(UuidShortener.class);
 
     @Autowired
     public UuidShortener(UrlRepository repository) {
@@ -18,21 +24,34 @@ public class UuidShortener implements ShortenerService{
     }
 
     @Override
-    public String generateShortUrl(String originalUrl) {
+    public UrlDTO generateShortUrl(String originalUrl) {
         try {
             String safeUrl = UrlSanitizer.sanitizeUrl(originalUrl);
 
             Optional<Url> existingUrl = repository.findByOriginalUrl(safeUrl);
             if (existingUrl.isPresent()) {
-                return existingUrl.get().getShortUrl();
+                logger.info("Requested URL already exists in database, returning the existing URL");
+                return UrlMapper.toDTO(existingUrl.get());
             }
 
-            String shortUrl = UUID.randomUUID().toString().replace("-", "").substring(0, 6);
+            logger.info("URL does not exist in database so creating a new one");
+            Url newUrl = new Url();
+            newUrl.setOriginalUrl(safeUrl);
+            newUrl.setCreatedAt(LocalDateTime.now());
+            // Setting the generated short URL
+            newUrl.setShortUrl(UUID.randomUUID().toString().replace("-", "").substring(0, 6));
 
-            Url generatedUrl = new Url(safeUrl, shortUrl);
-            repository.save(generatedUrl);
+            logger.info("Saving the URL to database before returning the DTO");
+            try {
+                newUrl = repository.save(newUrl);
+            } catch (DataAccessException e) {
+                logger.info("Error while saving URL to database", e);
+                throw new RuntimeException("Error while saving URL to database", e);
+            }
 
-            return shortUrl;
+            logger.info("Successfully saved URL to database");
+            logger.info("Successfully generated the short URL: {} for Original URL: {}", newUrl.getOriginalUrl(), safeUrl);
+            return UrlMapper.toDTO(newUrl);
         } catch (IllegalArgumentException e) {
             throw new RuntimeException("URL Validation error: " + e.getMessage());
         } catch (Exception e) {
