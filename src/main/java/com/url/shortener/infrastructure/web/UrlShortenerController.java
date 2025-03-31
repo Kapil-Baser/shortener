d3fd71c6ee988f83b22fd82dd7ac705f9f5006fa
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-03-31 12:18:31 +0530

    Changed the method name which now correctly displays the purpose of the method.

--- src/main/java/com/url/shortener/infrastructure/web/UrlShortenerController.java
+++ src/main/java/com/url/shortener/infrastructure/web/UrlShortenerController.java
@@ -1,8 +1,11 @@
 package com.url.shortener.infrastructure.web;
 
+import com.url.shortener.domain.dto.UrlDTO;
 import com.url.shortener.domain.factory.UrlShorteningFactory;
+import com.url.shortener.domain.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,15 +13,24 @@ import org.springframework.web.bind.annotation.*;
 public class UrlShortenerController {
 
     private final UrlShorteningFactory urlShorteningFactory;
+    private final UrlService urlService;
 
     @Autowired
-    public UrlShortenerController(UrlShorteningFactory factory) {
+    public UrlShortenerController(UrlShorteningFactory factory, UrlService service) {
         this.urlShorteningFactory = factory;
+        this.urlService = service;
     }
 
     @PostMapping("/shorten")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String shortenUrl(@RequestParam String url, @RequestParam(defaultValue = "base62") String strategy) {
-        return urlShorteningFactory.generateShortUrl(url, strategy);
+/*    @ResponseStatus(HttpStatus.CREATED)*/
+    public ResponseEntity<UrlDTO> shortenUrl(@RequestParam String url, @RequestParam(defaultValue = "base62") String strategy) {
+        UrlDTO dto = urlShorteningFactory.generateShortUrl(url, strategy);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
+
+    @GetMapping("/shorten/{url}")
+    public ResponseEntity<UrlDTO> getUrlDTOFromShortUrl(@PathVariable("url") String url) {
+        UrlDTO dtoFromOriginalUrl = urlService.getDTOFromShortUrl(url);
+        return ResponseEntity.status(HttpStatus.OK).body(dtoFromOriginalUrl);
     }
 }
