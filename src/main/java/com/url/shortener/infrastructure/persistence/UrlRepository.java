37884c4c012f0488aec46659ebb7052411c0c1ed
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-04-01 17:36:03 +0530

    Renamed originalUrl to just url

--- src/main/java/com/url/shortener/infrastructure/persistence/UrlRepository.java
+++ src/main/java/com/url/shortener/infrastructure/persistence/UrlRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
-    Optional<Url> findByOriginalUrl(String originalUrl);
+    Optional<Url> findByUrl(String url);
     Optional<Url> findByShortUrl(String shortUrl);
 }
