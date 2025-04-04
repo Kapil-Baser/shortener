44ae1ed7f352fa7941aa95671730ce4bfab4c6f9
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-04-04 17:07:00 +0530

    ...

--- src/main/java/com/url/shortener/domain/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.url.shortener.domain.exception;
-
-public class UrlNotFoundException extends UrlServiceException{
-    public UrlNotFoundException(String message) {
-        super(message);
-    }
-}
