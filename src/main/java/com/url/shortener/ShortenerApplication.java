51ab17343a07d6d2bfbf360ad58751f835dbf099
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-04-13 15:11:45 +0530

    ...

--- src/main/java/com/url/shortener/ShortenerApplication.java
+++ src/main/java/com/url/shortener/ShortenerApplication.java
@@ -1,8 +1,7 @@
-package com.url.shortener.main;
+package com.url.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class ShortenerApplication {
--- src/test/java/com/url/shortener/ShortenerApplicationTests.java
+++ src/test/java/com/url/shortener/ShortenerApplicationTests.java
@@ -3,7 +3,7 @@ package com.url.shortener;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ShortenerApplicationTests.class)
 class ShortenerApplicationTests {
 
 	@Test
