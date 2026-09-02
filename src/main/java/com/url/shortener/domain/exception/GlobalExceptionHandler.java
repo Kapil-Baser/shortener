916a06e1d4a46e4e56e94600cc23d97386897ef6
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-09-02 12:57:18 +0530

    Refactored to make use of ProblemDetail and removed the database exception method.

--- src/main/java/com/url/shortener/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/url/shortener/domain/exception/GlobalExceptionHandler.java
@@ -1,29 +1,66 @@
 package com.url.shortener.domain.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.net.URI;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String TIMESTAMP = "timestamp";
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest) {
-        ErrorResponse resourceNotFoundResponse = new ErrorResponse(ex.getMessage(), webRequest.getDescription(false));
-        return new ResponseEntity<>(resourceNotFoundResponse,HttpStatus.NOT_FOUND);
+    public ResponseEntity<ProblemDetail> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        problemDetail.setTitle("Resource not found.");
+        problemDetail.setType(URI.create("/errors/resource-not-found"));
+        problemDetail.setProperty(TIMESTAMP, Instant.now());
+
+        return ResponseEntity.of(problemDetail).build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                "Your request payload has validation errors.");
+
+        problemDetail.setTitle("Validation Failed.");
+        problemDetail.setType(URI.create("/errors/validation-failed"));
+
+        Map<String, String> validationErrors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            FieldError fieldError = (FieldError) error;
+            String fieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+            validationErrors.put(fieldName, errorMessage);
+        });
+
+        problemDetail.setProperty("errors", validationErrors);
+        problemDetail.setProperty(TIMESTAMP, Instant.now());
+
+        return ResponseEntity.of(problemDetail).build();
     }
 
     @ExceptionHandler(InvalidUrlException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidUrlException(InvalidUrlException ex, WebRequest webRequest) {
-        ErrorResponse invalidUrlResponse = new ErrorResponse(ex.getMessage(), webRequest.getDescription(false));
-        return new ResponseEntity<>(invalidUrlResponse, HttpStatus.BAD_REQUEST);
-    }
+    public ResponseEntity<ProblemDetail> handleInvalidUrlException(InvalidUrlException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                ex.getMessage());
 
-    @ExceptionHandler(DataBaseException.class)
-    public ResponseEntity<ErrorResponse> handleDataBaseException(DataBaseException ex, WebRequest webRequest) {
-        ErrorResponse dataBaseErrorResponse = new ErrorResponse(ex.getMessage(), webRequest.getDescription(false));
-        return new ResponseEntity<>(dataBaseErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        problemDetail.setTitle("Validation Failed.");
+        problemDetail.setType(URI.create("/errors/invalid-url"));
+        problemDetail.setProperty(TIMESTAMP, Instant.now());
+
+        return ResponseEntity.of(problemDetail).build();
     }
 }
