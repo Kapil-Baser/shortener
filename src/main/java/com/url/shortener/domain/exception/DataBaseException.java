316eba68d2b51bf406b2a9adefe3200ebd9b10a6
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-09-02 13:07:20 +0530

    Removed the unused error response since now I am using the ProblemDetail
    Removed the unused database exception

--- src/main/java/com/url/shortener/domain/exception/DataBaseException.java
+++
@@ -1,7 +0,0 @@
-package com.url.shortener.domain.exception;
-
-public class DataBaseException extends UrlServiceException{
-    public DataBaseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
