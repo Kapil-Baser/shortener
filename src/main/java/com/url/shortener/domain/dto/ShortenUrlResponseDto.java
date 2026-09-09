35cca9364b9dd44a80d5f0587b0e67a47b001b92
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-09-09 10:48:30 +0530

    Added Swagger document annotations to the Dto

--- src/main/java/com/url/shortener/domain/dto/ShortenUrlResponseDto.java
+++ src/main/java/com/url/shortener/domain/dto/ShortenUrlResponseDto.java
@@ -1,6 +1,22 @@
 package com.url.shortener.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
-public record ShortenUrlResponseDto(String id, String url, String shortCode, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record ShortenUrlResponseDto(
+        @Schema(
+                example = "1"
+        )
+        String id,
+        @Schema(
+                example = "https://example.com/some-long-url"
+        )
+        String url,
+        @Schema(
+                example = "abc123"
+        )
+        String shortCode,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt) {
 }
