80de94d43c454ae6a29e54a483dedde9e12df221
Kapil-Baser <kapil.kumar.baser@gmail.com>
2026-09-09 10:49:03 +0530

    Added Swagger document annotations to the controller

--- src/main/java/com/url/shortener/infrastructure/web/controller/UrlShortenerController.java
+++ src/main/java/com/url/shortener/infrastructure/web/controller/UrlShortenerController.java
@@ -5,6 +5,8 @@ import com.url.shortener.domain.dto.ShortenUrlResponseDto;
 import com.url.shortener.domain.dto.UrlStatsDto;
 import com.url.shortener.domain.service.UrlService;
 import com.url.shortener.domain.service.UrlShortenerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,10 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/shorten")
+@Tag(
+        name = "Url Shortner Controller",
+        description = "Controller for creating, updating, deleting and getting a short code for a long url and an endpoint for getting stats for a short code."
+)
 public class UrlShortenerController {
 
     private final UrlShortenerService shortenerService;
@@ -27,6 +33,7 @@ public class UrlShortenerController {
         this.urlService = service;
     }
 
+    @Operation(description = "API responsible for creating a short code of provided url.")
     @PostMapping
     public ResponseEntity<ShortenUrlResponseDto> shortenUrl(@Valid @RequestBody ShortenUrlRequestDto requestDto, UriComponentsBuilder ucb) {
         ShortenUrlResponseDto dto = shortenerService.generateShortUrl(requestDto);
@@ -37,24 +44,28 @@ public class UrlShortenerController {
         return ResponseEntity.created(locationOfNewUrl).body(dto);
     }
 
+    @Operation(description = "API responsible for fetching the original url from it's short code.")
     @GetMapping("/{shortCode}")
     public ResponseEntity<ShortenUrlResponseDto> getOriginalUrl(@PathVariable("shortCode") String shortCode) {
         var responseDto = urlService.getUrlDto(shortCode);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
+    @Operation(description = "API responsible for updating an existing url.")
     @PutMapping("/{shortCode}")
     public ResponseEntity<ShortenUrlResponseDto> updateUrl(@PathVariable("shortCode") String shortCode, @Valid @RequestBody ShortenUrlRequestDto dto) {
         var updatedUrl = urlService.updateUrl(shortCode, dto);
         return ResponseEntity.status(HttpStatus.OK).body(updatedUrl);
     }
 
+    @Operation(description = "API responsible for deleting a url.")
     @DeleteMapping("/{shortCode}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUrl(@PathVariable("shortCode") String shortCode) {
         urlService.deleteByShortCode(shortCode);
     }
 
+    @Operation(description = "API responsible for returning the stats for a url.")
     @GetMapping("/{shortCode}/stats")
     public ResponseEntity<UrlStatsDto> urlStats(@PathVariable("shortCode") String shortCode) {
         var stats = urlService.getStats(shortCode);
